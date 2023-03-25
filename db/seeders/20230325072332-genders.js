206b1270588a9ef036e4e96493ff02dc189c85dd
VietAnh188 <anhkun123456@gmail.com>
2023-03-25 14:33:19 +0700

    seeder gender

---
+++ db/seeders/20230325072332-genders.js
@@ -0,0 +1,37 @@
+'use strict';
+const { v4 } = require('uuid')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const genders = [
+      'male',
+      'female',
+      'other'
+    ]
+
+    await queryInterface.bulkInsert('Genders', genders.map(gender => ({
+      id: v4(),
+      name: gender
+    })))
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Genders', null, {})
+  }
+};
