ea19c009e64886367bf162554dc6f5f6c4b08688
VietAnh188 <anhkun123456@gmail.com>
2023-04-08 00:45:41 +0700

    upload file

---
+++ src/modules/upload/upload.controller.ts
@@ -0,0 +1,70 @@
+import {
+  Controller,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { v4 } from 'uuid';
+import { bucket } from 'firebase';
+
+@Controller('upload')
+export class UploadController {
+  @Post(':folder')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile() file: Express.Multer.File,
+    @Param() params: any,
+    @Res() response: any,
+  ) {
+    try {
+      const { folder } = params;
+
+      if (!file) {
+        response
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: 'file not found' });
+      }
+
+      const id = v4();
+      const blob = bucket.file(folder + '/' + id + '_' + file.originalname);
+      const blobWriter = blob.createWriteStream({
+        metadata: {
+          contentType: file.mimetype,
+          metadata: {
+            firebaseStorageDownloadTokens: id,
+          },
+        },
+      });
+
+      blobWriter.on('error', (error) => {
+        response.status(HttpStatus.FORBIDDEN).json({
+          message: error,
+        });
+      });
+
+      blobWriter.on('finish', () => {
+        const fileUrl =
+          'gs://gobackpacking-baf39.appspot.com/' +
+          bucket.name +
+          '/o/' +
+          encodeURIComponent(blob.name) +
+          '?alt=media&token' +
+          id;
+        response.status(HttpStatus.OK).json({
+          fileUrl,
+          fileName: folder + '/' + id + '_' + file.originalname,
+        });
+      });
+
+      blobWriter.end(file.buffer);
+    } catch (error) {
+      response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: 'something went wrong!',
+      });
+    }
+  }
+}
